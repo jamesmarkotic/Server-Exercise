d36c387435f2d7f7aae6e40d963ffd6edf0f9c03
jamesmarkotic <jamesmarkotic@gmail.com>
2018-02-06 19:37:23 -0800

    tiny modification

--- twoserver.js
+++ twoserver.js
@@ -1,5 +1,7 @@
 // Require/import the HTTP module
 var http = require("http");
+var nice = [];
+var mean = [];
 
 // Define a port to listen for incoming requests
 var PORT = 7000;
@@ -8,13 +10,13 @@ var PORTTWO = 7500;
 function handleRequestOne(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
-  response.end("You have great hair today!" + request.url);
+  response.end("You have great hair!");
 }
 
 function handleRequestTwo(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
-  response.end("You're adopted." + request.url);
+  response.end("You're adopted.");
 }
 
 // Use the Node HTTP package to create our server.
@@ -24,16 +26,13 @@ var servertwo = http.createServer(handleRequestTwo);
 
 // Start our server so that it can begin listening to client requests.
 server.listen(PORT, function() {
-  console.log('Hey it\'s working!');
-  console.log('You have nice hair today!');
 
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
 
 servertwo.listen(PORTTWO, function() {
-  console.log('Hey it\'s working!');
-  console.log('You\'re adopted.');
+
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORTTWO);
 });
